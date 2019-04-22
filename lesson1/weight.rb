125d012704f5d688325366d6438019c0ee5ceb98
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-04-22 21:00:51 +0300

    Update weight.rb

--- lesson1/weight.rb
+++ lesson1/weight.rb
@@ -1,9 +1,9 @@
 puts "Whats your name?"
 name = gets.chomp
 puts "What is your height?"
-height = gets.chomp.to_i
+height = gets.to_i
 puts "what is your weight?"
-weight = gets.chomp.to_i
+weight = gets.to_i
 if height - 110 <= weight
   puts "#{name}, your height is #{height} and u should go to gym"
 else
