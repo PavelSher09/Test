354b9c2b1b4b347725a6b4ad7963ade0218775ca
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-04-22 20:56:53 +0300

    Update pifagor.rb

--- lesson1/pifagor.rb
+++ lesson1/pifagor.rb
@@ -2,10 +2,14 @@ puts "enter sides A B C"
 
 sides = gets.chomp.split(' ').map(&:to_f)
 a, b, c = sides.sort
-puts "Треугольник прямоугольный" if c**2 == a**2 + b**2
-puts "Треугольник равнобедренный" if (a == b) || (b == c) || (a == c)
-puts " и равносторонний" if (a == b) && (b == c) && (a == c)
 
-
+right_triangle = c**2 == a**2 + b**2
+if right_triangle && a == b
+  puts 'прямоугольный и равнобедренный'
+elsif right_triangle 
+  puts 'прямоугольный'
+elsif a == c
+  puts 'равносторонний'
+end
 
 		
