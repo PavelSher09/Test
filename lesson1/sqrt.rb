7b3e7a8fe96743edc5a170797b1ce8fc048867e9
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-04-22 15:29:08 +0300

    Update sqrt.rb

--- lesson1/sqrt.rb
+++ lesson1/sqrt.rb
@@ -4,16 +4,17 @@ coef = gets.chomp.split(' ').map(&:to_f)
 a, b, c = coef
 
 
-d = b ** 2 - 4 * a * c
+d = b**2 - 4 * a * c
 
 
 
 if d > 0
   d_sqrt = Math.sqrt(d)
-  puts x1 = (-b + d_sqrt) / (2.0 * a)
-  puts x2 = (-b - d_sqrt) / (2.0 * a)
+  x1 = (-b + d_sqrt) / (2.0 * a)
+  x2 = (-b - d_sqrt) / (2.0 * a)
+  puts "x1 = #{x1}, x2 = #{x2}"
 elsif d == 0
-  puts x = - b / (2.0 * a)
+  puts x = -b / (2.0 * a)
 elsif d < 0
   puts "No roots"
 end 
