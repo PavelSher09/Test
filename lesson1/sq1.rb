43925dd4f1174738629491b119042789ca7e7f96
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-04-22 20:59:44 +0300

    Update sq1.rb

--- lesson1/sq1.rb
+++ lesson1/sq1.rb
@@ -1,6 +1,6 @@
 puts "enter basement"
-basement = gets.chomp.to_f
+basement = gets.to_f
 puts "enter height"
-height = gets.chomp.to_f
+height = gets.to_f
 square = 0.5 * basement * height
 puts "the square is #{square}"
