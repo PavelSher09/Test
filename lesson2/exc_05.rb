fdaf5182400b1a8188448142f1a88c4c94f6726c
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-04-24 19:30:13 +0300

    Update exc_05.rb

--- lesson2/exc_05.rb
+++ lesson2/exc_05.rb
@@ -1,11 +1,12 @@
-monthes  = [31,28,31,30,31,30,31,31,30,31,30,31]
+months  = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 puts "enter day, month, year"
 dates = gets.chomp.split(' ').map(&:to_i)
 date, month, year = dates
 
 if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
- monthes[1] = 29
+  months[1] = 29
 end
 
-puts date + monthes.take( month - 1 ).sum
+day_order = date + months.take(month - 1).sum
+puts day_order
