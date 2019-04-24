f64cdd68fc33314eff0c58e9fbe0af8a3d04f312
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-04-24 19:28:43 +0300

    Update exc_03.rb

--- lesson2/exc_03.rb
+++ lesson2/exc_03.rb
@@ -1,10 +1,8 @@
-
-  
 fibs = [0,1]
-nxt = 1
+next_number = 1
 
-while nxt < 100
-  fibs << nxt
-  nxt = fibs[-2] + fibs[-1]
+while next_number < 100
+  fibs << next_number
+  next_number = fibs[-2] + fibs[-1]
 end
 puts fibs
