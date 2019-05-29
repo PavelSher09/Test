8e08e2f6512ca3ea7158022b8398d91ad317c296
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-05-29 18:46:18 +0300

    Update route.rb

--- Lesson3/route.rb
+++ Lesson3/route.rb
@@ -1,1 +1,21 @@
+class Route
 
+  attr_reader :stations, :name
+
+  def initialize(start_station, end_station)
+     @stations = [start_station, end_station]
+  end
+  
+  def add_route(stations)
+     @stations.insert(-2, stations)
+  end
+
+  def delete_route(station)
+    @stations.delete(station) unless [stations.first, stations.last].include?(station)
+  end
+
+  def show_stations
+    stations.each { |station| puts station.name }
+  end
+
+end
