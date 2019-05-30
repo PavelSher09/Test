5bd47c6323bdeff842fcdac095b27d8bd76b4de1
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-05-30 11:11:48 +0300

    Update station.rb

--- Lesson3/station.rb
+++ Lesson3/station.rb
@@ -14,9 +14,9 @@ class Station
   def send_train(train)
     @trains.delete(train)
   end
-
+  
   def train_by_type(type)
-    @trains.select { |train| train.get_type == type }  
+    @trains.select { |train| train.type == type }  
   end
   
 end
