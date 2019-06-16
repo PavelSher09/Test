5b53dd18c9b6a0bfb5f262238a4a67d42be047f4
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-06-16 17:11:04 +0300

    Update run.rb

--- Lesson4/run.rb
+++ Lesson4/run.rb
@@ -3,7 +3,7 @@ require_relative 'train'
 require_relative 'station'
 require_relative 'route'
 require_relative 'passenger_train'
-require_relative 'pass_wagon'
+require_relative 'passenger_wagon'
 require_relative 'cargo_train'
 require_relative 'cargo_wagon'
 
