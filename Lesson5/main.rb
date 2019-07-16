81c85ba03f7ddca708f86d008c601f6e7cd7834d
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-07-16 22:28:48 +0300

    Add files via upload

---
+++ Lesson5/main.rb
@@ -0,0 +1,193 @@
+require_relative 'train'
+require_relative 'station'
+require_relative 'route'
+require_relative 'passenger_train'
+require_relative 'cargo_train'
+require_relative 'instance_counter'
+require_relative 'manufacturer'
+
+class Main
+
+attr_reader  :stations,  :routes,  :trains
+
+  def initialize
+    @stations = []
+    @routes = []
+    @trains = []
+  end
+
+  def run
+
+    loop do
+      show_main_menu
+      action = gets.to_i
+      case action
+      when 1 then create_station
+      when 2 then create_train
+      when 3 then add_wagon
+      when 4 then create_route
+      when 5 then control_route
+      when 7 then get_route
+      when 8 then go
+      when 9 then show_stations_trains
+      when 10 then show_stations
+      when 11 then show_routes
+      when 12 then delete_wagon
+      when 0 then break
+      end
+    end
+  end
+
+  def show_main_menu
+    puts '1 - New station'
+    puts '2 - New train'
+    puts '3 - Add wagon'
+    puts '4 - New route'
+    puts '5 - Add\delete stations to route'
+    puts '7 - Add train to route'
+    puts '8 - Follow route'
+    puts '9 - Show station trains'
+    puts '10 - Show stations'
+    puts '11 - Show routes'
+    puts '12 - Delete wagon'
+    puts '0 - Exit'
+  end
+
+  def show(list)
+    list.each.with_index(1) { |item, index| puts "#{index} - #{item}" }
+  end
+
+  def select_from_list(list)
+    i = gets.to_i
+    list[i - 1]
+  end
+
+  def create_station
+    puts "What's the name of station?"
+    name = gets.chomp
+    @stations << Station.new(name)
+  end
+
+  def create_train
+    puts "What's the number of train?"
+    number = gets.chomp
+    puts "Choose type of train: 1 - passenger, 2 - cargo"
+    type = gets.to_i
+    case type
+    when 1 then @trains << PassengerTrain.new(number)
+    when 2 then @trains << CargoTrain.new(number)
+    end
+    show(@trains)
+  end
+
+  def show_routes
+    show(routes)
+  end
+
+  def add_wagon
+    puts "What train to attach?"
+    show(@trains)
+    train = select_from_list(@trains)
+    case train
+    when PassengerTrain then train.add_wagon(PassengerWagon.new)
+    when CargoTrain then train.add_wagon(CargoWagon.new)
+    end
+    puts "To train #{train.number} attached wagon"
+    show(train.wagons)
+  end
+
+  def delete_wagon
+    puts "From what train to delete wagon"
+    show(@trains)
+    train = select_from_list(@trains)
+    show(train.wagons)
+    puts "select wagon to delete"
+    selected_wagon = select_from_list(train.wagons)
+    train.delete_wagon(selected_wagon)
+    show(train.wagons)
+  end
+
+  def create_route
+    puts "Set start station"
+    show(@stations)
+    start = select_from_list(@stations)
+    puts "Set finish station"
+    finish = select_from_list(@stations)
+    @routes << Route.new(start, finish)
+    show(@routes)
+  end
+
+  def control_route
+    puts "Choose index of route to opperate with"
+    show(@routes)
+    route = select_from_list(routes)
+    puts route
+    puts "Choose what to do. 1 - add station to route; 2 - delete station from route"
+    case gets.to_i
+    when 1 then add_station_to_route(route)
+    when 2 then delete_station_from_route(route)
+    end
+  end
+
+  def add_station_to_route(route)
+    puts "Select index of route to add station"
+    show(@routes)
+    route_index = select_from_list(@routes)
+    puts "Enter index of station to add"
+    show(@stations)
+    station_index = select_from_list(@stations)
+    route_index.add_route(station_index)
+    show(route.stations)
+  end
+
+  def delete_station_from_route(route)
+    puts "Select index of route to delete station"
+    show(@routes)
+    route_index = select_from_list(@routes)
+    puts "Enter index of station to delete"
+    show(@stations)
+    station_index = select_from_list(@stations)
+    route_index.delete_route(station_index)
+    show(route.stations)
+  end
+
+  def get_route
+    puts "Select train to get route"
+    show(@trains)
+    train = select_from_list(@trains)
+    puts "Select route"
+    show(@routes)
+    route = select_from_list(@routes)
+    train.get_route(route)
+  end
+
+  def go
+    puts "Select train to move"
+    show(@trains)
+    train = select_from_list(@trains)
+    puts "Select where to move: 1 - next; 2 - back"
+    case  gets.to_i
+    when 1 then train.go_next
+    when 2 then train.go_back
+    end
+    puts train.current_station
+  end
+
+  def show_stations_trains
+    puts "Select station to show"
+    show(@stations)
+    station = select_from_list(@stations)
+    puts station.trains
+  end
+
+  def show_stations
+    show(@stations)
+  end
+
+  def to_s
+    number
+  end
+
+end
+
+Main.new.run
---
+++ Lesson5/train.rb
@@ -0,0 +1,84 @@
+require_relative 'instance_counter'
+require_relative 'manufacturer'
+
+class Train
+
+include Manufacturer
+include InstanceCounter
+
+attr_reader :route,  :speed,  :wagons,  :type, :number
+
+@@trains = {}
+
+  def self.find(train_number)
+    @@trains[train_number]
+  end
+
+  def initialize(number)
+    @number = number
+    @type = type
+    @speed = 0
+    @wagons = []
+    @@trains[number] = self
+    register_instance
+  end
+
+  def go(value)
+    return if value < 0
+    @speed = value
+  end
+
+
+  def stop
+    @speed = 0
+  end
+
+def add_wagon(wagon)
+  return unless speed.zero?
+  return unless attachable_wagon?(wagon)
+  @wagons << wagon
+end
+
+def delete_wagon(wagon)
+  return unless speed.zero?
+  return unless attachable_wagon?(wagon)
+  @wagons.delete(wagon)
+end
+
+def get_route(route)
+    @route = route
+    @current_station_index = 0
+    current_station.add_train(self)
+  end
+
+  def current_station
+    @route.stations[@current_station_index]
+  end
+
+  def go_next
+    return if next_station.nil?
+    current_station.send_train(self)
+    next_station.add_train(self)
+    @current_station_index += 1
+  end
+
+  def go_back
+    return if previous_station.nil?
+    current_station.send_train(self)
+    previous_station.add_train(self)
+    @current_station_index -= 1
+  end
+
+  def next_station
+    route.stations[@current_station_index + 1]
+  end
+
+  def previous_station
+    route.stations[@current_station_index - 1] if @current_station_index - 1 >= 0
+  end
+
+  def to_s
+    number
+  end
+
+end
