59a7927edc6f1ad28f79aa25c409060d87aa6eae
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-09-28 16:27:51 +0300

    Add files via upload

--- Lesson9/cargo_train.rb
+++ Lesson9/cargo_train.rb
@@ -1,7 +1,15 @@
 require_relative 'train'
 require_relative 'cargo_wagon'
+require_relative 'validation'
+require_relative 'accessors'
 
 class CargoTrain < Train
+
+  include Validation
+  include Accessors
+
+  validate  :number, :format, NUMBER_FORMAT
+
   def initialize(number)
     @type = :cargo
     super
